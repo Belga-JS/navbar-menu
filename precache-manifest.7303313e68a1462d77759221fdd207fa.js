6d21aa6cb69ca46efc5ac664a172f1f9af2e8d3c
Belga-JS <Meherzi.belga@yahoo.com>
2019-11-21 15:40:02 +0100

    Updates

---
+++ precache-manifest.7303313e68a1462d77759221fdd207fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4977e61ec1835bba0e377c8bf3ba1e",
+    "url": "/navbar-menu/index.html"
+  },
+  {
+    "revision": "7b18ed3a384ed8488e1f",
+    "url": "/navbar-menu/static/css/main.9ff10da7.chunk.css"
+  },
+  {
+    "revision": "1c02ea5ec3d0f4262c69",
+    "url": "/navbar-menu/static/js/2.2811d590.chunk.js"
+  },
+  {
+    "revision": "7b18ed3a384ed8488e1f",
+    "url": "/navbar-menu/static/js/main.72f6df7a.chunk.js"
+  },
+  {
+    "revision": "fe78aa553e9d7f275614",
+    "url": "/navbar-menu/static/js/runtime-main.ab1f0576.js"
+  }
+]);
